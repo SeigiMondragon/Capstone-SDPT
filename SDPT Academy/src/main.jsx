6dd12336260e6a48925c264210c6ab43805260e6
SamuelAlac <samuelalac21@gmail.com>
2025-03-12 21:04:03 +0800

    main

--- SDPT Academy/src/main.jsx
+++ SDPT Academy/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRoute, RouterProvider} from 'react-router-dom'
 
+const Router = createBrowserRoute([
+{
+  path: "/",
+  element: <App/>
+},
+
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {Router}/>
   </StrictMode>,
 )
